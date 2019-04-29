d64e0432d743b76b2e96e54484e96653d4dd5358
Kelson Warner <kelson@null.net>
2019-04-28 21:06:11 -0700

    update to random facts

--- index.js
+++ index.js
@@ -3,20 +3,17 @@ import express from 'express';
 import twilio from 'twilio';
 import bodyParser from 'body-parser';
 import db from './db';
-
+import Wikifakt from 'wikifakt';
 const PORT = 3000;
 const app = express();
 // const client = twilio(TWILIO_SID, TWILIO_AUTH);
 app.use(bodyParser.urlencoded({ extended: false }));
-app.post('/sms', (req, res) => {
+app.post('/sms', async (req, res) => {
   const twiml = new twilio.twiml.MessagingResponse();
   const { From, Body } = req.body;
 
-  twiml.message(`Nugget`);
-  twiml.media(
-    `https://media.npr.org/assets/img/2017/05/09/10-piece-chicken-nuggets-ss_0_custom-db31f599b4b36050d9a26986abaf75c76c655f37-s800-c85.jpg`
-  );
-
+  const fact = await Wikifakt.getRandomFact();
+  twiml.message(fact);
   res.writeHead(200, { 'Content-Type': 'text/xml' });
   res.end(twiml.toString());
 });
